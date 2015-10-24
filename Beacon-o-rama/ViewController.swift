b694da10175be87662b7085cdc0607504c2f72ca
Christian H. Mosveen <mosveen@gmail.com>
2015-10-24 14:24:59 +0200

    Initial Commit

---
+++ Beacon-o-rama/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beacon-o-rama
+//
+//  Created by Christian H. Mosveen on 24/10/15.
+//  Copyright © 2015 BEKK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
